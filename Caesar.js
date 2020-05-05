666b6f7770011e112e7bb6b5721065748583a0c4
faahiero <fabriciosilvalp@outlook.com>
2020-05-04 23:47:49 -0300

    first commit

---
+++ Caesar.js
@@ -0,0 +1,24 @@
+function rot13(s, n) {
+    s.toLowerCase();
+    const arr = s.split('')
+    var solved = "";
+    arr.map((item)=> {
+        var asciiNum = item.charCodeAt();
+        if(asciiNum < 97 || asciiNum > 97+25) {
+            solved+=item;
+        } else {
+            if(asciiNum-n < 97){
+                let v = (asciiNum - n) - 96; 
+                if(v < 0) v = -v;
+                asciiNum = 97+(25-v);
+                solved+=String.fromCharCode(asciiNum);
+            } else {
+                solved+=String.fromCharCode(asciiNum-n);
+            }
+        }
+
+    });
+    return solved;
+}
+
+module.exports = rot13
