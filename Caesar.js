64acbafa16681074fe3050d2530569c4b3b7917d
faahiero <fabriciosilvalp@outlook.com>
2020-05-11 18:02:35 -0300

    projeto finalizado

--- Caesar.js
+++ Caesar.js
@@ -1,24 +1,23 @@
-function rot13(s, n) {
+function caesarCipher(s, n) {
     s.toLowerCase();
     const arr = s.split('')
     var solved = "";
-    arr.map((item)=> {
+    arr.map((item) => {
         var asciiNum = item.charCodeAt();
-        if(asciiNum < 97 || asciiNum > 97+25) {
-            solved+=item;
+        if (asciiNum < 97 || asciiNum > 97 + 25) {
+            solved += item;
         } else {
-            if(asciiNum-n < 97){
-                let v = (asciiNum - n) - 96; 
-                if(v < 0) v = -v;
-                asciiNum = 97+(25-v);
-                solved+=String.fromCharCode(asciiNum);
+            if (asciiNum - n < 97) {
+                let v = (asciiNum - n) - 96;
+                if (v < 0) v = -v;
+                asciiNum = 97 + (25 - v);
+                solved += String.fromCharCode(asciiNum);
             } else {
-                solved+=String.fromCharCode(asciiNum-n);
+                solved += String.fromCharCode(asciiNum - n);
             }
         }
-
     });
     return solved;
 }
 
-module.exports = rot13
+module.exports = caesarCipher
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 const decifra = require('./Caesar.js')
 const sha1 = require('js-sha1');
-let fs = require('fs');
+const fs = require('fs');
 const token = "cc165f62d6fd57a977491bbe397f631a4bca785e"
-const streamToBlob = require('stream-to-blob');
-const Blob = require('blob');
-const formData = require('form-data')
+const FormData = require('form-data')
 
-function getPosts() {
+
+function getJson() {
     axios
         .get(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`)
         .then((res) => {
@@ -16,66 +15,24 @@ function getPosts() {
             const decifrado = decifra(cifrado, numCasas);
             res["data"]["decifrado"] = decifrado;
             res["data"]["resumo_criptografico"] = sha1(decifrado);
-            // fs.writeFile("./answer.json", JSON.stringify(res["data"], null, 4), function (err) { console.log('Arquivo Criado') });
-            sendFile(res["data"])
+            fs.writeFile("./answer.json", JSON.stringify(res["data"], null, 4), function (err) { console.log('Arquivo Criado') });
+            sendJsonFile();
         })
         .catch((err) => {
             console.error(err)
         });
 };
 
-async function sendFile(obj) {
-    let data = new formData()
-    data.append('numero_casas', obj.numero_casas)
-    data.append('token', obj.token)
-    data.append('cifrado', obj.cifrado)
-    data.append('decifrado', obj.decifrado)
-    data.append('resumo_criptografico', obj.resumo_criptografico)
-    // console.log(data)
-    // let formDataToBufferObject = formDataToBuffer( data );
-    axios.post(`https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`, data, {
-        headers: {
-            'Content-Type': 'multipart/form-data',
-        }
-    }).then((res) => console.log(res)).catch((err) => console.log(err));
+
+async function sendJsonFile() {
+    const formData = new FormData();
+    formData.append('answer', fs.createReadStream('./answer.json'));
+    const res = await axios.post(`https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`, formData,
+        {
+            // You need to use `getHeaders()` in Node.js because Axios doesn't
+            // automatically set the multipart form boundary in Node.
+            headers: formData.getHeaders()
+        }).then((res) => console.log(res)).catch((err) => console.log(err));
 }
 
-getPosts()
-
-
-
-
-function formDataToBuffer( formData ) {
-    let dataBuffer = new Buffer( 0 );
-    let boundary   = formData.getBoundary();
-    for( let i = 0, len = formData._streams.length; i < len; i++ ) {
-
-        if( typeof formData._streams[i] !== 'function' ) {
-
-            dataBuffer = bufferWrite( dataBuffer, formData._streams[i] );
-
-            if( typeof formData._streams[i] !== 'string' || formData._streams[i].substring( 2, boundary.length + 2 ) !== boundary ) {
-                dataBuffer = bufferWrite( dataBuffer, "\r\n" );
-            }
-        }
-    }
-
-    // Close the request
-    dataBuffer = bufferWrite( dataBuffer, '--' + boundary + '--' );
-
-    return dataBuffer;
-}
-
-// Below function appends the data to the Buffer.
-function bufferWrite( buffer, data ) {
-
-    let addBuffer;
-    if( typeof data === 'string' ) {
-        addBuffer = Buffer.from( data );
-    }
-    else if( typeof data === 'object' && Buffer.isBuffer( data ) ) {
-        addBuffer = data;
-    }
-
-    return Buffer.concat( [buffer, addBuffer] );
-}
+getJson()
